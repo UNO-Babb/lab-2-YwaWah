00c7ec40f486daa18480c5fadc7128064731b776
YwaWah <ywah@unomaha.edu>
2025-01-29 15:28:51 +0000

    random random

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -10,8 +10,20 @@ def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
   #Prompt the user for their question.
-
+  answers = ["Yes", "Yes-definetely", "No", "Very Doubtful", "Ask Again Later", "Cannot Predict Now", "It is certain",
+              "Most Likely", "I say no", "Outlook good", "Outlook bad", "Not so sure", 
+              "it is certain", "It is not certain", "You may rely on it", "You may not like it"]
   #Answer question randomly with one of the options from your earlier list.
+  question = input("Ask a question: ")
+  num = random.random()
+  num = num * 1000
+  num = int(num)
+  num = num % 15
+  print(answers[num])
+  #print(answers[16])
+
+  response = random.choice(answers)
+  print(response)
 
 
 if __name__ == '__main__':
